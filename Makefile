7bf7b91126cbdbec58f54c5238460784e5608357
Unia <jthidskes@live.nl>
2012-12-19 00:40:55 +0100

    combined both music players into one

--- Makefile
+++ Makefile
@@ -1,5 +1,23 @@
-dwm-status: dwmst.c
-	gcc -o dwmst dwmst.c `pkg-config --cflags --libs x11 alsa glib-2.0 dbus-glib-1 audclient` -liw
+PROG     =  dwmst
+PREFIX   =  /usr/local
+
+# MPD, comment if you don't want it
+MPDLIB   =  -lmpdclient
+MPDFLAG  =  -DMPD
+
+# AUDACIOUS, comment if you don't want it
+#AUDLIB	 =  -ldbus-glib-1 -lglib-2.0 -laudclient
+AUDLIB	 =	`pkg-config --cflags --libs glib-2.0 dbus-glib-1 audclient`
+AUDFLAG  =  -DAUD
+
+LIBS     =  -liw -lasound -lX11 ${MPDLIB} ${AUDLIB}
+CPPFLAGS =  ${MPDFLAG} ${AUDFLAG}
+CFLAGS   =  -Os -Wall -Wno-unused-parameter -Wno-unused-result ${CPPFLAGS}
+
+
+$(PROG): $(PROG).c
+	@$(CC) $(CFLAGS) $(LIBS) -o $(PROG) $(PROG).c
+	@strip $(PROG)
 
 clean:
-	rm dwmst
+	rm -f $(PROG)
--- dwmst.c
+++ dwmst.c
@@ -4,53 +4,75 @@
 #include <string.h>
 #include <time.h>
 #include <locale.h>
-#include <alsa/asoundlib.h>
-//#include <audacious/audctrl.h>
-//#include <audtool.h>
-#include <audacious/dbus.h>
 #include <X11/Xlib.h>
+#ifdef MPD
+#include <mpd/client.h>
+#endif
+#ifdef AUD
+#include <audacious/dbus.h>
+#include <audacious/audctrl.h>
+#endif
 #include <iwlib.h>
+#include <alsa/asoundlib.h>
 
 #define WIFI			"wlan0"		// Wireless interface
 #define BATT_LOW		11			// Below BATT_LOW percentage left on battery, the battery display turns red
 #define INTERVAL		1			// Sleeps for INTERVAL seconds between updates
 #define VOL_CH			"Master"	// Channel to watch for volume
 // Files read for system info:
+#define SKYPE_FILE		"/home/jente/.Skype/jente_etnej/main.lock"
 #define BATT_NOW		"/sys/class/power_supply/BAT0/charge_now"
 #define BATT_FULL		"/sys/class/power_supply/BAT0/charge_full"
 #define BATT_STAT		"/sys/class/power_supply/BAT0/status"
 // Display format strings. Defaults make extensive use of escape characters for colors which require colorstatus patch.
-#define MUSIC_STR		"%s   \x02•\x01   "							// Music, playing
-#define MUSIC_P_STR		"Paused\x02:\x01 %s   \x02•\x01   "			// Music, paused
-#define MUSIC_S_STR		" "											// Music, stopped
-#define NO_MUSIC_STR	"Geen verbinding    \x02•\x01   "			// Music, can't connect
+#ifdef MPD
+#define MPD_STR         "%s \x02-\x01 %s   \x02•\x01   "                    // MPD, playing
+#define MPD_P_STR       "Paused\x02:\x01 %s \x02-\x01 %s   \x02•\x01   "    // MPD, paused
+#define MPD_S_STR       " "                                                 // MPD, stopped
+#define NO_MPD_STR      "Geen verbinding    \x02•\x01   "                   // MPD, can't connect
+#endif
+#ifdef AUD
+#define MUSIC_STR       "%s   \x02•\x01   "                         // Music, playing
+#define MUSIC_P_STR     "Paused\x02:\x01 %s   \x02•\x01   "         // Music, paused
+#define MUSIC_S_STR     " "                                         // Music, stopped
+#define NO_MUSIC_STR    "Geen verbinding    \x02•\x01   "           // Music, can't connect
+#endif
+#define SKYPE_STR		"Skype   \x02•\x01   "						// Skype is running
+#define NO_SKYPE_STR	" "											// Skype is not running
 #define WIFI_STR		" %s %d%%   "								// WIFI
 #define NO_WIFI_STR		"  Geen verbinding   "						// WIFI, no connection
 #define VOL_STR			"\x02•\x01   %d%%   "						// Volume
 #define VOL_MUTE_STR	"\x02•\x01       ×       "					// Volume, muted
-#define BAT_STR			"\x02•\x01   D %d%%   "						// Battery, BAT, above BATT_LOW percentage
-#define BAT_LOW_STR		"\x02•\x03   D %d%%   "						// Battery, BAT, below BATT_LOW percentage
-#define BAT_FULL_STR	"\x02•\x04   F \x01%d%%   "					// Battery, full
-#define BAT_CHRG_STR	"\x02•\x01   C %d%%   "						// Battery, AC
+#define BAT_STR			"\x02•\x01   D  %d%%   "					// Battery, BAT, above BATT_LOW percentage
+#define BAT_LOW_STR		"\x02•\x03   D  %d%%   "					// Battery, BAT, below BATT_LOW percentage
+#define BAT_FULL_STR	"\x02•\x04   F  \x01%d%%   "				// Battery, full
+#define BAT_CHRG_STR	"\x02•\x01   C  %d%%   "					// Battery, AC
 #define DATE_TIME_STR	"\x02•\x01   %a %d %b\x02,\x01 %H:%M   "	// This is a strftime format string which is passed localtime
 
 int main() {
 	Display *dpy;
 	Window root;
-	int num, skfd, has_bitrate=0, loops=60;
-	long lnum1,lnum2;
-	char statnext[30], status[100], wifiString[30];
-	gint playpos;
-	gchar *psong;
-	DBusGProxy *session = NULL;
-	DBusGConnection *connection = NULL;
-	session = 0;
-	psong = NULL;
+	int num, skfd, wifiloops=60, skypeloops=20, musicloops=10;
+	long lnum1, lnum2;
+	char statnext[100], status[100], wifistring[30], skypestring[30], musicstring[100];
+#ifdef MPD
+    struct mpd_song * song = NULL;
+    const char * title = NULL;
+    const char * artist = NULL;
+#endif
+#ifdef AUD
+    gint playpos;
+    gchar *psong;
+    DBusGProxy *session = NULL;
+    DBusGConnection *connection = NULL;
+    session = 0;
+    psong = NULL;
+#endif
 	struct wireless_info *winfo;
 	winfo = (struct wireless_info *) malloc(sizeof(struct wireless_info));
 	memset(winfo, 0, sizeof(struct wireless_info));
 	long vol = 0, max = 0, min = 0;
-	int mute = 0;
+	int mute = 0, realvol = 0;
 	time_t current;
 	FILE *infile;
 	// Setup X display and root window id:
@@ -61,36 +83,92 @@ int main() {
 	}
 	root = XRootWindow(dpy,DefaultScreen(dpy));
 	setlocale(LC_ALL, "");
+#ifdef AUD
+	g_type_init();
+#endif
 // MAIN LOOP STARTS HERE
 	for (;;) {
 		status[0]='\0';
-	// Music
-		g_type_init();
-		connection = dbus_g_bus_get(DBUS_BUS_SESSION, NULL);
-		session = dbus_g_proxy_new_for_name(connection, AUDACIOUS_DBUS_SERVICE, AUDACIOUS_DBUS_PATH, AUDACIOUS_DBUS_INTERFACE);
-		playpos = audacious_remote_get_playlist_pos(session);
-		psong = audacious_remote_get_playlist_title(session, playpos);
-		if (psong) {
-			if (audacious_remote_is_paused(session)) {
-				sprintf(statnext,MUSIC_P_STR,psong);
-				g_free(psong);
-				psong = NULL;
-			} else if (audacious_remote_is_playing(session)) {
-				sprintf(statnext,MUSIC_STR,psong);
-				g_free(psong);
-				psong = NULL;
-			} else {
-				sprintf(statnext,MUSIC_S_STR);
+	// MUSIC
+		//if (++musicloops > 10) {
+		//	musicloops=0;
+#ifdef MPD
+			struct mpd_connection * conn = mpd_connection_new(NULL, 0, 30000);
+    	    if (mpd_connection_get_error(conn))
+        	    sprintf(statnext,NO_MPD_STR);
+
+			mpd_command_list_begin(conn, true);
+            mpd_send_status(conn);
+            mpd_send_current_song(conn);
+            mpd_command_list_end(conn);
+
+			struct mpd_status* theStatus = mpd_recv_status(conn);
+                if (!theStatus)
+                    sprintf(statnext,NO_MPD_STR);
+                else
+                    if (mpd_status_get_state(theStatus) == MPD_STATE_PLAY) {
+                        mpd_response_next(conn);
+                        song = mpd_recv_song(conn);
+                        title = mpd_song_get_tag(song, MPD_TAG_TITLE, 0);
+                        artist = mpd_song_get_tag(song, MPD_TAG_ARTIST, 0);
+                        sprintf(statnext,MPD_STR,title,artist);
+                        mpd_song_free(song);
+                    }
+                    else if (mpd_status_get_state(theStatus) == MPD_STATE_PAUSE) {
+                        mpd_response_next(conn);
+						song = mpd_recv_song(conn);
+                        title = mpd_song_get_tag(song, MPD_TAG_TITLE, 0);
+                        artist = mpd_song_get_tag(song, MPD_TAG_ARTIST, 0);
+                        sprintf(statnext,MPD_P_STR,title,artist);
+                        mpd_song_free(song);
+                    }
+                    else if (mpd_status_get_state(theStatus) == MPD_STATE_STOP) {
+                        sprintf(statnext,MPD_S_STR);
+                    }
+            mpd_response_finish(conn);
+            mpd_connection_free(conn);
+			strcat(status,statnext);
+#endif
+#ifdef AUD
+        	connection = dbus_g_bus_get(DBUS_BUS_SESSION, NULL);
+        	session = dbus_g_proxy_new_for_name(connection, AUDACIOUS_DBUS_SERVICE, AUDACIOUS_DBUS_PATH, AUDACIOUS_DBUS_INTERFACE);
+        	playpos = audacious_remote_get_playlist_pos(session);
+        	psong = audacious_remote_get_playlist_title(session, playpos);
+        	if (psong) {
+	            if (audacious_remote_is_paused(session)) {
+    	            sprintf(statnext,MUSIC_P_STR,psong);
+        	        g_free(psong);
+            	    psong = NULL;
+	            } else if (audacious_remote_is_playing(session)) {
+    	            sprintf(statnext,MUSIC_STR,psong);
+        	        g_free(psong);
+            	    psong = NULL;
+	            } else {
+    	            sprintf(statnext,MUSIC_S_STR);
+        	    }
+	        } else {
+    	        sprintf(statnext,MUSIC_S_STR);
+        	}
+	        g_object_unref(session);
+    	    session = NULL;
+			strcat(status,statnext);
+#endif
+		//}
+	// SKYPE
+		if (++skypeloops > 20) {
+			skypeloops=0;
+			infile = fopen(SKYPE_FILE,"r");
+			if (infile) {
+				sprintf(skypestring,SKYPE_STR);
+				fclose(infile);
 			}
-		} else {
-			sprintf(statnext,MUSIC_S_STR);
+			else
+				sprintf(skypestring,NO_SKYPE_STR);
 		}
-		g_object_unref(session);
-		session = NULL;
-		strcat(status,statnext);
+		strcat(status,skypestring);
 	// WIFI
-		if (++loops > 60) {
-			loops=0;
+		if (++wifiloops > 60) {
+            wifiloops=0;
 			skfd = iw_sockets_open();
 			if (iw_get_basic_config(skfd, WIFI, &(winfo->b)) > -1) {
 				if (iw_get_stats(skfd, WIFI, &(winfo->stats), // set present winfo variables
@@ -102,9 +180,9 @@ int main() {
 				}
 				if (winfo->b.has_essid) {
 					if (winfo->b.essid_on) {
-						sprintf(wifiString,WIFI_STR,winfo->b.essid,(winfo->stats.qual.qual*100)/winfo->range.max_qual.qual);
+						sprintf(wifistring,WIFI_STR,winfo->b.essid,(winfo->stats.qual.qual*100)/winfo->range.max_qual.qual);
 					} else {
-						sprintf(wifiString,NO_WIFI_STR);
+						sprintf(wifistring,NO_WIFI_STR);
 					}
 				}
 			}
@@ -112,7 +190,7 @@ int main() {
 			fflush(stdout);
 			memset(winfo, 0, sizeof(struct wireless_info));
 		}
-		strcat(status,wifiString);
+		strcat(status,wifistring);
 	// Audio volume
 		snd_mixer_t *handle; // init alsa
 		snd_mixer_open(&handle, 0);
@@ -134,7 +212,8 @@ int main() {
 		if(mute == 0)
 			sprintf(statnext, VOL_MUTE_STR);
 		else
-			sprintf(statnext, VOL_STR, !!mute * (vol*100)/max);
+			realvol = (vol*100)/max;
+			sprintf(statnext, VOL_STR, realvol);
 
 		if(vol_info)
 			snd_mixer_selem_id_free(vol_info);
