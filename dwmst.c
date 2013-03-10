78b8daf5c44d17bd45f6966c860cb71ee0587841
Unia <jthidskes@live.nl>
2013-03-10 23:53:27 +0100

    added remaining time to battery function

--- dwmst.c
+++ dwmst.c
@@ -18,7 +18,8 @@
 #endif
 
 #define WIFI			"wlan0"		/* Wireless interface */
-#define BATT_LOW		11			/* Below BATT_LOW percentage left on battery, the battery display turns red */
+#define BATT_LOW_P		11			/* Below BATT_LOW percentage left on battery, the battery display turns red */
+#define BATT_LOW_T		3			/* Same as above, but now minutes instead of percentage */
 #define INTERVAL		1			/* Sleeps for INTERVAL seconds between updates */
 #define VOL_CH			"Master"	/* Channel to watch for volume */
 /* Files read for system info: */
@@ -26,12 +27,14 @@
 #define BATT_NOW		"/sys/class/power_supply/BAT0/charge_now"
 #define BATT_FULL		"/sys/class/power_supply/BAT0/charge_full"
 #define BATT_STAT		"/sys/class/power_supply/BAT0/status"
+#define BATT_VOLT		"/sys/class/power_supply/BAT0/voltage_now"
+#define BATT_CNOW		"/sys/class/power_supply/BAT0/current_now"
 /* Display format strings. Defaults make extensive use of escape characters for colors which require colorstatus patch. */
 #ifdef MPD
-#define MPD_STR			"\x02%s \x01 - \x02 %s  "		/* MPD, playing */
+#define MPD_STR			"\x02%s \x01 - \x02 %s  "			/* MPD, playing */
 #define MPD_P_STR		"Paused: \x02 %s \x01 - \x02 %s  "	/* MPD, paused */
-#define MPD_S_STR		""								/* MPD, stopped */
-#define NO_MPD_STR		"Geen verbinding  "				/* MPD, can't connect */
+#define MPD_S_STR		""									/* MPD, stopped */
+#define NO_MPD_STR		"Geen verbinding  "					/* MPD, can't connect */
 #endif
 #ifdef AUD
 #define MUSIC_STR		"\x02%s  "						/* Music, playing */
@@ -44,10 +47,10 @@
 #define NO_WIFI_STR		"\x01 Geen verbinding "			/* WIFI, no connection */
 #define VOL_STR			"\x01 Volume \x02 %d%% "		/* Volume */
 #define VOL_MUTE_STR	"\x01 Volume \x02 × "			/* Volume, muted */
-#define BAT_STR			"\x01 Batterij \x02 D %d%%"		/* Battery, BAT, above BATT_LOW percentage */
-#define BAT_LOW_STR		"\x01 Batterij \x02 D %d%%"		/* Battery, BAT, below BATT_LOW percentage */
 #define BAT_FULL_STR	"\x01 Batterij \x02 F %d%%"		/* Battery, full */
-#define BAT_CHRG_STR	"\x01 Batterij \x02 C %d%%"		/* Battery, AC */
+#define BAT_STR			"\x01 Batterij \x02 D %d%%, %02d:%02d resterend"		/* Battery, discharging, above BATT_LOW percentage */
+#define BAT_LOW_STR		"\x01 Batterij \x02 D %d%%, %02d:%02d resterend"		/* Battery, discharging, below BATT_LOW percentage */
+#define BAT_CHRG_STR	"\x01 Batterij \x02 C %d%%, %02d:%02d tot opgeladen"	/* Battery, AC */
 #ifdef CLK
 #define DATE_TIME_STR	"\x01 %a \x02%d \x01%b,\x02 %H:%M"	/* This is a strftime format string which is passed localtime */
 #endif
@@ -56,8 +59,9 @@ int main() {
 	Display *dpy;
 	Window root;
 	FILE *infile;
-	int num, skfd, mute=0, realvol=0, wifiloops=60, musicloops=10;
-	long lnum1, lnum2, vol=0, max=0, min=0;
+	int perc, skfd, mute = 0, realvol = 0, wifiloops = 60, musicloops = 10;
+	int hours, minutes, seconds = -1;
+	long now = -1, full = -1, voltage = -1, rate = -1, vol = 0, max = 0, min = 0;
 	char statnext[100], status[200], wifistring[30], musicstring[100];
 	struct wireless_info *winfo;
 	snd_mixer_t *handle;
@@ -101,7 +105,7 @@ int main() {
 	/* MUSIC */
 #ifdef MPD
 		if (++musicloops > 10) {
-			musicloops=0;
+			musicloops = 0;
 			struct mpd_connection * conn = mpd_connection_new(NULL, 0, 30000);
 			if (mpd_connection_get_error(conn))
 				sprintf(musicstring, NO_MPD_STR);
@@ -141,7 +145,7 @@ int main() {
 #endif
 #ifdef AUD
 		if (++musicloops > 10) {
-			musicloops=0;
+			musicloops = 0;
 			connection = dbus_g_bus_get(DBUS_BUS_SESSION, NULL);
 			session = dbus_g_proxy_new_for_name(connection, AUDACIOUS_DBUS_SERVICE, AUDACIOUS_DBUS_PATH, AUDACIOUS_DBUS_INTERFACE);
 			playpos = audacious_remote_get_playlist_pos(session);
@@ -177,7 +181,7 @@ int main() {
 		strcat(status, statnext);
 	/* WIFI */
 		if (++wifiloops > 60) {
-			wifiloops=0;
+			wifiloops = 0;
 			skfd = iw_sockets_open();
 			if (iw_get_basic_config(skfd, WIFI, &(winfo->b)) > -1) {
 				if (iw_get_stats(skfd, WIFI, &(winfo->stats), /* set present winfo variables */
@@ -189,7 +193,7 @@ int main() {
 				}
 				if (winfo->b.has_essid) {
 					if (winfo->b.essid_on) {
-						sprintf(wifistring, WIFI_STR, winfo->b.essid, (winfo->stats.qual.qual*100)/winfo->range.max_qual.qual);
+						sprintf(wifistring, WIFI_STR, winfo->b.essid, (winfo->stats.qual.qual * 100) / winfo->range.max_qual.qual);
 					} else {
 						sprintf(wifistring, NO_WIFI_STR);
 					}
@@ -218,7 +222,7 @@ int main() {
 		if(mute == 0) {
 			sprintf(statnext, VOL_MUTE_STR);
 		} else {
-			realvol = (vol*100)/max;
+			realvol = (vol * 100) / max;
 			sprintf(statnext, VOL_STR, realvol);
 		}
 		if(vol_info)
@@ -230,22 +234,42 @@ int main() {
 		strcat(status,statnext);
 	/* Power / Battery */
 		infile = fopen(BATT_NOW, "r");
-			fscanf(infile, "%ld\n", &lnum1); fclose(infile);
+			fscanf(infile, "%ld\n", &now); fclose(infile);
 		infile = fopen(BATT_FULL, "r");
-			fscanf(infile, "%ld\n", &lnum2); fclose(infile);
+			fscanf(infile, "%ld\n", &full); fclose(infile);
 		infile = fopen(BATT_STAT, "r");
 			fscanf(infile, "%s\n", statnext); fclose(infile);
-		num = (lnum1*100)/lnum2;
-		if (strncmp(statnext, "Charging", 8) == 0) {
-			sprintf(statnext, BAT_CHRG_STR, num);
-		} else if (strncmp(statnext, "Full", 8) == 0) {
-				sprintf(statnext, BAT_FULL_STR, num);
+		infile = fopen(BATT_VOLT, "r");
+			fscanf(infile, "%ld\n", &voltage); fclose(infile);
+		infile = fopen(BATT_CNOW, "r");
+			fscanf(infile, "%ld\n", &rate); fclose(infile);
+		now = ((float)voltage / 1000) * ((float)now / 1000);
+		full = ((float)voltage / 1000) * ((float)full / 1000);
+		rate = ((float)voltage / 1000) * ((float)rate / 1000);
+		perc = (now * 100) / full;
+
+		if (strncmp(statnext, "Full", 8) == 0) {
+			sprintf(statnext, BAT_FULL_STR, perc);
+		} else if (strncmp(statnext, "Charging", 8) == 0) {
+			seconds = 3600 * (((float)full - (float)now) / (float)rate);
+			hours = seconds / 3600;
+			seconds -= 3600 * hours;
+			minutes = seconds / 60;
+			seconds -= 60 * minutes;
+			sprintf(statnext, BAT_CHRG_STR, perc, hours, minutes); 
 		} else {
-			if (num <  BATT_LOW)
-				sprintf(statnext, BAT_LOW_STR, num);
-			else
-				sprintf(statnext, BAT_STR, num);
+			seconds = 3600 * ((float)now / (float)rate);
+            hours = seconds / 3600;
+			seconds -= 3600 * hours;
+			minutes = seconds / 60;
+			seconds -= 60 * minutes;
+			if (perc <  BATT_LOW_P || minutes < BATT_LOW_T) {				
+				sprintf(statnext, BAT_LOW_STR, perc, hours, minutes);
+			} else {
+				sprintf(statnext, BAT_STR, perc, hours, minutes);
+			}
 		}
+		hours = minutes = seconds = 0;
 		strcat(status, statnext);
 	/* Time */
 #ifdef CLK
